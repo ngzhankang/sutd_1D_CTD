e902ee2fa45e790b16cd949bc4d82561be35e51c
Shiro-ken <Shiro-ken@domain.com>
2024-11-22 16:25:26 +0800

    changed grades dict to json file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from helperClass import card
 import json
-from datafiles import info
 
 # import turtle
 
@@ -17,11 +16,11 @@ from datafiles import info
 # if module_taken == "Physical World" or "PW":
 #     Mod = Module("10.015", module_taken, "12")
 
-with open('info.json', 'r') as file:
+with open('datafiles\info.json', 'r') as file:
     courses = json.load(file)
 
-grade_dict = {"A+":5.3, "A":5.0, "A-":4.5, "B+":4.0, "B":3.5, "B-":3.0, "C+":2.5, "C":2.0, "C-":1.5, "D":1.0, "F":0.0}
 
+grade_dict = {"A+":5.3, "A":5.0, "A-":4.5, "B+":4.0, "B":3.5, "B-":3.0, "C+":2.5, "C":2.0, "C-":1.5, "D":1.0, "F":0.0}
 
 total_credit = (grade_dict[grade]*12) + (grade_dict[grade]*12) + (grade_dict[grade]*12) + (grade_dict[grade]*12)
 GPA = total_credit/(4*12)
