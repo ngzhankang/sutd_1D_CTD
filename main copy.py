f45dd83bbe3e8f74c0e77c0678c4d1fefc30a281
Shiro-ken <Shiro-ken@domain.com>
2024-12-09 22:35:17 +0800

    yes

--- main copy.py
+++ main copy.py
@@ -154,7 +154,7 @@ class App(ttk.Frame):
             button = tk.Button(
                 self.cards_frame, 
                 text=f"{card[0]}\n({card[1]})",
-                width=28,  # Fixed width
+                width=self.window_width//70,  # Fixed width
                 height=5,  # Fixed height
                 command=lambda c=card: self.select_card(c, self.selected),
                 bg = 'white',
